914b26d47f1c89c7b186887003c48d5a02a7ac5c
AnubhabDey-TransEV <transmogrify21@outlook.com>
2024-11-04 15:21:19 +0530

    Added API Rate Limiting

--- main.py
+++ main.py
@@ -28,6 +28,8 @@ from contextlib import asynccontextmanager
 from wallet_methods import create_wallet_route, delete_wallet, recharge_wallet, edit_wallet, debit_wallet, get_wallet_recharge_history, get_wallet_transaction_history
 from wallet_api_models import UserIDRequest, RechargeWalletRequest, EditWalletRequest, DebitWalletRequest
 from playhouse.shortcuts import model_to_dict
+from collections import defaultdict
+from datetime import timedelta
 
 
 asyncio.set_event_loop(uvloop.new_event_loop())
@@ -57,7 +59,7 @@ async def get_ip_information(ip_address: str):
     except Exception as e:
         print(f"Failed to retrieve IP information for {ip_address}: {e}")
         return None
-    
+
 class VerifyAPIKeyMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         if request.url.path.startswith("/api/"):
@@ -68,6 +70,37 @@ class VerifyAPIKeyMiddleware(BaseHTTPMiddleware):
                 raise HTTPException(status_code=403, detail="Invalid API key")
         response = await call_next(request)
         return response
+
+class RateLimitMiddleware(BaseHTTPMiddleware):
+    def __init__(self, app: FastAPI, rate_limit: int = int(config("RPS")), time_window: int = int(config("TWS"))):
+        super().__init__(app)
+        self.rate_limit = rate_limit  # Max requests allowed
+        self.time_window = timedelta(seconds=time_window)  # Time window in seconds
+        self.requests = defaultdict(list)  # Tracks requests per IP
+        self.lock = asyncio.Lock()  # Ensures safe access to `requests`
+
+    async def dispatch(self, request: Request, call_next):
+        client_ip = request.client.host
+        current_time = datetime.now()
+
+        async with self.lock:
+            # Filter out old requests outside the time window
+            self.requests[client_ip] = [
+                timestamp for timestamp in self.requests[client_ip]
+                if current_time - timestamp < self.time_window
+            ]
+
+            # Enforce rate limit
+            if len(self.requests[client_ip]) >= self.rate_limit:
+                return JSONResponse(
+                    status_code=429,
+                    content={"detail": "Too many requests"},
+                )
+
+            # Record the current request time
+            self.requests[client_ip].append(current_time)
+
+        return await call_next(request)
 
 # Middleware class for API request and response tracking
 class APITrackingMiddleware(BaseHTTPMiddleware):
@@ -134,6 +167,7 @@ app = FastAPI(lifespan=lifespan)
 middleware = [
     Middleware(APITrackingMiddleware),
     Middleware(VerifyAPIKeyMiddleware),
+    Middleware(RateLimitMiddleware),
     Middleware(
         CORSMiddleware,
         allow_origins=['*'],
